fb73145c68b3c0d6dfb138a01f01e320f4dbd488
arunavajana99 <arunavajana47@gmail.com>
2024-02-06 00:07:45 +0530

    custom api response and error handling

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,28 @@
-const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 export { asyncHandler };
+
+//try-catch method ==========>
+
+// const asyncHandler = () => {};
+
+// const asyncHandler = (fn) => {
+//   () => {};
+// };
+
+// const asyncHandler = (fn) => async () => {};
+
+// const asyncHandler = (fn) =>async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// };
